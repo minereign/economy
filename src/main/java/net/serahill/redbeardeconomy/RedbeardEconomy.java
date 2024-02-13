8603091f83219ab6d112d73f994c73294ecaeba7
RayaSerahill <ville@daini.dev>
2024-02-13 18:23:00 +0200

    init

---
+++ src/main/java/net/serahill/redbeardeconomy/RedbeardEconomy.java
@@ -0,0 +1,48 @@
+package net.serahill.redbeardeconomy;
+
+import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.security.Permission;
+
+public final class RedbeardEconomy extends JavaPlugin implements CommandExecutor {
+
+    private RedbeardEconomy redbeardEconomy;
+    private static Economy econ = null;
+    private static final Permission perms = null;
+    private static final Chat chat = null;
+
+    @Override
+    public void onEnable() {
+
+        if (!setupEconomy() ) {
+            getLogger().severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        this.saveDefaultConfig();
+        this.reloadConfig();
+
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return true;
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/net/serahill/redbeardeconomy/trasureHandler.java
@@ -0,0 +1,5 @@
+package net.serahill.redbeardeconomy;
+
+public class trasureHandler {
+
+}
